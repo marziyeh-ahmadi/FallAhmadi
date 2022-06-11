fb8133fa3a0fc8fc70596f2a91ec17ca935dc4a2
marziyeh-ahmadi <ahmadi0021.2002@gmail.com>
2022-06-11 18:55:37 +0430

    کالی گرافی برای فونت کاستوم اضافه شد

---
+++ app/src/main/java/com/example/fall/MyApplication.java
@@ -0,0 +1,21 @@
+package com.example.fall;
+
+import android.app.Application;
+
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/Dima_Shekasteh.ttf")
+                                .setFontAttrId(R.attr.fontPath)
+                                .build()))
+                .build());
+    }
+}
